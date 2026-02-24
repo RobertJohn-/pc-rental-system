ba7430cd64bfeb03e5dd1a247209e4be4f54114d
Robert John <robertjohn.millano@gmail.com>
2026-02-24 17:58:32 +0800

    feat: Updated IBookingRepository property

--- backend/src/PcRental.Application/Interfaces/Repositories/IBookingRepository.cs
+++ backend/src/PcRental.Application/Interfaces/Repositories/IBookingRepository.cs
@@ -3,7 +3,7 @@ using PcRental.Domain.Entities;
 public interface IBookingRepository
 {
     Task<Booking> AddAsync(Booking booking);
-    Task<Booking> GetByIdAsync(int id);
+    Task<Booking?> GetByIdAsync(int id);
     Task UpdateAsync(Booking booking);
     Task<List<Booking>> GetActiveBookingAsync();
 }
